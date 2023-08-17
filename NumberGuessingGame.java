29407d63afbd99b1edbbb0110c02b1658322a046
ajshinde05 <108792755+ajshinde05@users.noreply.github.com>
2023-08-17 21:27:28 +0530

    comments

---
+++ NumberGuessingGame.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 50;
+        int maxAttempts = 5;
+        int score = 0;
+        
+        System.out.println("Welcome to the Number Guessing Game!");
+        boolean playAgain = true;
+        
+        while (playAgain) {
+            int targetNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+            
+            System.out.println("I'm thinking of a number between " + minRange + " and " + maxRange);
+            
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+                
+                if (userGuess == targetNumber) {
+                    System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+                    guessedCorrectly = true;
+                    score++;
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+            
+            if (!guessedCorrectly) {
+                System.out.println("Sorry, you've used all " + maxAttempts + " attempts. The number was: " + targetNumber);
+            }
+            
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainInput = scanner.next();
+            playAgain = playAgainInput.equalsIgnoreCase("yes");
+        }
+        
+        System.out.println("Game over! Your final score is: " + score);
+    }
+}
